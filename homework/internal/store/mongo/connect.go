5f24bda09eb7b17d5f4d5901794098a78936f7a7
Thanh Tran <thanhtran@chotot.vn>
2023-09-06 09:15:02 +0700

    feat(TODO 1): Set ReadPreference for mongo client

--- homework/internal/store/mongo/connect.go
+++ homework/internal/store/mongo/connect.go
@@ -6,12 +6,17 @@ import (
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func Connect(mongoURI string, dbName string) (*mongo.Database, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
+
+	clientOptions := options.Client().ApplyURI(mongoURI)
+	// Set the read preference
+	clientOptions.SetReadPreference(readpref.Primary())
+	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		panic(err)
 	}
